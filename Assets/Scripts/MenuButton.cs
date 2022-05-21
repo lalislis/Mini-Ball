86cee03d6e8faa0310fd213562bfd4c2c9c2e8b8
Muhammad Luthfi <mluthfitaufiq@gmail.com>
2022-05-22 01:27:25 +0700

    Change goal collider in basketball

--- Assets/Scripts/MenuButton.cs
+++ Assets/Scripts/MenuButton.cs
@@ -5,16 +5,30 @@ using UnityEngine;
 public class MenuButton : MonoBehaviour
 {
     [SerializeField] GameObject pauseUI;
+    [SerializeField] GameObject gameOverUI;
+    [SerializeField] ChangeScene changeScene;
 
     public void pauseGame()
     {
-        Time.timeScale = 0f;
-        pauseUI.SetActive(true);
+        if (isGameOver())
+        {
+            changeScene.LoadScene("Main Menu");
+        }
+        else
+        {
+            Time.timeScale = 0f;
+            pauseUI.SetActive(true);
+        }
     }
 
     public void resumeGame()
     {
         Time.timeScale = 1f;
         pauseUI.SetActive(false);
+    }
+
+    private bool isGameOver()
+    {
+        return gameOverUI.activeSelf;
     }
 }
